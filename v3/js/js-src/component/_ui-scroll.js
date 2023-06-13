b68ee16cf508bde23bc9fe5d333a4b55861ee8a5
005387eunjye <eunjye@uix.kr>
2023-06-13 17:40:06 +0900

    0613

---
+++ v3/js/js-src/component/_ui-scroll.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const uiScroll = () => {
+    $(".pop-scroll").mCustomScrollbar({
+        setHeight: 407
+    });
+    $(".cont-scroll").mCustomScrollbar({
+        setHeight: 307
+    });
+} ;
+export {uiScroll};
